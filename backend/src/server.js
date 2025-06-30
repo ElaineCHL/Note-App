15a3fb96d02c3994ff28c05318a0df1cbb1e600e
ElaineCHL <elainechunghuilin@gmail.com>
2025-06-30 13:48:48 +0800

    Init: Initialize backend with Express, MongoDB, and Upstash rate limiting

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import noteRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+connectDB();
+
+
+// middleware
+app.use(express.json()); // this middleware parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// simple custom middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use("/api/notes", noteRoutes)
+
+app.listen(PORT, () => {
+  console.log("Server started on PORT: " + PORT);
+});
